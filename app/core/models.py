f6a54c404901300866b60dba6e601dcd1e540a67
H Bazai <bazaee.hassan@gmail.com>
2023-02-13 11:02:02 +0330

    Recipe API created

--- app/core/models.py
+++ app/core/models.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import (
     PermissionsMixin
 )
 
+from django.conf import settings
 
 class UserManager(BaseUserManager):
     """ Manager for users """
@@ -43,3 +44,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+class Recipe(models.Model):
+    """ Recipe object """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
